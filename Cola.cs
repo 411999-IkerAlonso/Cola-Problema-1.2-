35b5f948366b99ac8cac34163937e16435fead61
Iker <411999@tecnicatura.frc.utn.edu.ar>
2024-11-13 01:16:48 -0300

    Cola Andando

---
+++ Cola.cs
@@ -0,0 +1,53 @@
+﻿using Problema_1._1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_1._2
+{
+    public class Cola : IColleccion
+    {
+        public List<object> objetos { get; set; }
+        public int Contador { get; set; }
+        public Cola()
+        {
+            objetos = new List<object>();
+            Contador = 0;
+        }
+        public bool Añadir(object objeto)
+        {
+            objetos.Add(objeto);
+            Contador++;
+            return true;
+        }
+
+        public bool EstaVacia()
+        {
+            if(objetos.Count() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object Extraer()
+        {
+            object primero = objetos.First();
+            if(!EstaVacia())
+            {
+                objetos.Remove(objetos.First());
+            }
+            return primero;
+        }
+
+        public object Primero()
+        {
+            return objetos.First(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Problema_1._2;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+//Problema 1.2:
+//Colas.Desarrollar una clase Cola que implemente la interfaz definida en el
+//problema anterior pero esta vez utilizando un objeto List. Tener en cuenta que una
+//Cola es una estructura FIFO (Primero en entrar primero en salir
+
+Cola cola = new Cola();
+
+cola.Añadir(5);
+cola.Añadir(8);
+cola.Añadir(19);
+
+Console.WriteLine(cola.Primero());
+Console.WriteLine(cola.EstaVacia());
+cola.Extraer();
+Console.WriteLine(cola.Primero());
